2b7e57dbdf78e97e07622f82c8e9a401fab07ea1
Solveig Lund Helland <solveig.lund.helland@gmail.com>
2020-02-16 17:45:19 +0100

    clean-up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import { AllPlants } from "./pages/all-plants/all-plants";
 import { AddPlant } from "./pages/add-plant/add-plant";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Header } from "./components/header";
-import { Image } from "./add-image/Image";
 import { UserCard } from "./user/user";
-import { Upload } from "./add-plant/upload";
-import { DiscoverLabels } from "./add-plant/discoverLabels";
 
 export default class App extends Component {
   render() {
@@ -16,12 +13,8 @@ export default class App extends Component {
         <Router>
           <Header />
           <Route exact path="/" component={AllPlants} />
-          <Route path="/myPlants" component={AllPlants} />
           <Route path="/addPlant" component={AddPlant} />
-          <Route path="/plants/:id/discover-labels" component={DiscoverLabels} />
-          <Route path="/Image" component={Image} />
           <Route path="/User" component={UserCard} />
-          <Route path="/upload" component={Upload} />
         </Router>
       </div>
     );
