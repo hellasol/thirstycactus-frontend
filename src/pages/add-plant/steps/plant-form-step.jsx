e8cf4d4c4e2576c5e29661025a7719e61207cb9a
Solveig Lund Helland <solveig.lund.helland@gmail.com>
2020-02-22 22:11:12 +0100

    fixed image compression

--- src/pages/add-plant/steps/plant-form-step.jsx
+++ src/pages/add-plant/steps/plant-form-step.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import api from "../../../api";
-import { Col, FormGroup, Label, Input } from "reactstrap";
+import { FormGroup, Button, Input } from "reactstrap";
+import Resizer from 'react-image-file-resizer';
+
 
 export function PlantFormStep({ onSuccess }) {
   const [image, setImage] = useState(null); 
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const plant = await api.plants.create({image});
+    
+    const plant = await api.plants.create({image: await compress(image)});
     onSuccess(plant)
   }
 
@@ -15,24 +18,33 @@ export function PlantFormStep({ onSuccess }) {
     <form encType="multipart/form-data" onSubmit={handleSubmit}>
     <div>
       <br/>
-      <h1>Upload a picture</h1>
+      <h1>Add Plant</h1>
       <br/>
-      <FormGroup row>
-        <Label for="image" sm={2}>
-          Image
-        </Label>
-        <Col sm={10}>
+      <FormGroup>
           <Input
             type="file"
             name="image"
             id="image"
             onChange={e => setImage(e.target.files[0])}
           />
-        </Col>
       </FormGroup>
       <br />
-      <button type="submit">Submit</button>
+      <Button type="submit">Submit</Button>
     </div>
   </form>
   );
+  }
+
+  function compress (image) {
+    return new Promise(resolve => Resizer.imageFileResizer (
+      image,
+       1024,
+       1024,
+       'JPEG',
+       75,
+       0,
+       resolve,
+       "blob"
+     ));
+    
   }
